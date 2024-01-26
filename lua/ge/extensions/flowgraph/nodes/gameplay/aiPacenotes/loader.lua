e026611b474384fdd61f8995f418e1cb7a205e35
Alex Bird <alexebird@gmail.com>
2024-01-25 22:20:13 -0800

    backport rallystage fg

--- lua/ge/extensions/flowgraph/nodes/gameplay/aiPacenotes/loader.lua
+++ lua/ge/extensions/flowgraph/nodes/gameplay/aiPacenotes/loader.lua
@@ -1,14 +1,15 @@
-local im  = ui_imgui
+-- local im  = ui_imgui
 local re_util = require('/lua/ge/extensions/editor/rallyEditor/util')
 
 local C = {}
-local logTag = 'aipacenotes-fg'
+local logTag = 'aip-fg-loader'
 
 C.name = 'AI Pacenotes Loader'
 C.description = 'Do necessary loading for AI Pacenotes.'
 C.color = re_util.aip_fg_color
 C.tags = {'aipacenotes'}
 C.category = 'once_p_duration'
+-- C.category = 'once'
 
 C.pinSchema = {
   -- { dir = 'in', type = 'flow',   name = 'flow',  description = 'Inflow for this node.' },
@@ -18,10 +19,12 @@ C.pinSchema = {
   { dir = 'in', type = 'number', name = 'searchN', description = 'Number of closest pacenotes to search when vehicle position is reset.', default = 5},
 
   -- { dir = 'out', type = 'flow', name = 'flow', description = 'Outflow from this node.' },
-  { dir = 'out', type = 'table', name = 'rallyManager', tableType = 'rallyManager', description = 'The RallyManager' },
+  -- { dir = 'out', type = 'table', name = 'rallyManager', tableType = 'rallyManager', description = 'The RallyManager' },
 }
 
+-- this is called when the flowgraph code node is created, not when the flowgraph runs.
 function C:init()
+  -- self:reset()
 end
 
 -- function C:drawCustomProperties()
@@ -32,33 +35,65 @@ end
 --   im.Text(txt)
 -- end
 
+local readyHit = false
 function C:reset()
-  log('D', 'wtf', 'running AIP loader')
-  self.rallyManager = require('/lua/ge/extensions/gameplay/rally/rallyManager')()
-  self.rallyManager:setup(
-    self.pinIn.vehId.value,
-    self.pinIn.damageThresh.value,
-    self.pinIn.searchN.value
-  )
+  log('D', logTag, 'running AIP Loader v2')
+  readyHit = false
+
+  extensions.unload('gameplay_aipacenotes')
+  extensions.load('gameplay_aipacenotes')
+
+  extensions.gameplay_aipacenotes.helloWorld()
+
+  extensions.gameplay_aipacenotes.initRallyManager()
+
+
+  -- self.rallyManager = require('/lua/ge/extensions/gameplay/rally/rallyManager')(
+  --
+  -- )
+  -- self.rallyManager:setup(
+  --   self.pinIn.vehId.value,
+  --   self.pinIn.damageThresh.value,
+  --   self.pinIn.searchN.value
+  -- )
 end
 
-function C:onNodeReset()
-  self.rallyManager = nil
-end
+-- function C:onNodeReset()
+  -- log('D', logTag, 'onNodeReset')
+  -- self.rallyManager = nil
+-- end
 
-function C:_executionStopped()
-  self.rallyManager = nil
+-- function C:_executionStopped()
+  -- log('D', logTag, '_executionStopped')
+  -- self.rallyManager = nil
+-- end
+
+function C:workOnce()
+  log('D', logTag, 'workOnce')
+  self:reset()
 end
 
 function C:work()
+  -- log('D', logTag, 'work')
   -- if self.pinIn.reset.value then
-  --   self.rallyManager = nil
+    -- self.rallyManager = nil
+    -- self:reset()
   -- end
 
-  if not self.rallyManager then
-    self:reset()
-    self.pinOut.rallyManager.value = self.rallyManager
+  -- if not self.rallyManager then
+    -- self:reset()
+    -- self.pinOut.rallyManager.value = self.rallyManager
+  -- end
+
+  if not readyHit then
+    local loaded = extensions.isExtensionLoaded("gameplay_aipacenotes")
+    if loaded and extensions.gameplay_aipacenotes.isReady() then
+      log('D', logTag, 'extension is ready')
+      readyHit = true
+    end
   end
+
+  self.pinOut.flow.value = false
 end
 
 return _flowgraph_createNode(C)
