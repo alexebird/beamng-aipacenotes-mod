36cd1d925e04571494955980a23e113aa10541f2
Alex Bird <alexebird@gmail.com>
2024-02-06 21:56:52 -0800

    add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# set -v
+
+main() {
+    if git diff-index --quiet HEAD --; then
+        # Working tree is clean
+        echo "$(git rev-parse HEAD)" > gitsha.txt
+    else
+        # Working tree is not clean
+        echo "$(git rev-parse HEAD)+dirty" > gitsha.txt
+    fi
+
+    zip -r ../$(basename `pwd`).zip ./* -x '*.git*'
+    rm -fv gitsha.txt
+}
+
+main "$@"
