adf813e11ddf63f835788c15d9048a3413207d2c
Alex Bird <alexebird@gmail.com>
2024-03-08 22:29:18 -0800

    i think i got precountdown working

--- lua/ge/extensions/flowgraph/nodes/gameplay/aiPacenotes/preCountdown.lua
+++ lua/ge/extensions/flowgraph/nodes/gameplay/aiPacenotes/preCountdown.lua
@@ -7,46 +7,59 @@ C.name = 'AI Pacenotes Pre-Countdown'
 C.description = 'Plays pre-countdown audio.'
 C.color = re_util.aip_fg_color
 C.tags = {'aipacenotes'}
--- C.category = 'once_p_duration'
+C.category = 'once_instant'
 
 C.pinSchema = {
-  { dir = 'in', type = 'flow', name = 'flow', description = 'Inflow for this node.' },
-  { dir = 'in', type = 'flow', name = 'reset', description = 'Reset the countdown.', impulse = true },
-  { dir = 'out', type = 'flow', name = 'flow', description = 'Outflow for this node.' },
+  -- { dir = 'in', type = 'flow', name = 'flow', description = 'Inflow for this node.' },
+  -- { dir = 'in', type = 'flow', name = 'reset', description = 'Reset the countdown.', impulse = true },
+  -- { dir = 'out', type = 'flow', name = 'flow', description = 'Outflow for this node.' },
+
+  -- { dir = 'out', type = 'flow', name = 'done', description = 'Outflow for this node.', impulse = true },
 }
 
-function C:reset()
-  self.workDone = false
-  self.pinOut.flow.value = false
-  self.pinIn.reset.value = false
+-- function C:reset()
+--   self.workDone = false
+--   self.pinOut.flow.value = false
+--   self.pinIn.reset.value = false
+-- end
+
+function C:workOnce()
+  local rallyManager = extensions.gameplay_aipacenotes.getRallyManager()
+  rallyManager.audioManager:enqueuePauseSecs(0.5)
+  rallyManager.audioManager:enqueueStaticPacenoteByName('firstnoteintro_1/c')
+  local pacenote = extensions.gameplay_aipacenotes.getRallyManager().notebook.pacenotes.sorted[1]
+  rallyManager.audioManager:enqueuePacenote(pacenote)
+  rallyManager.audioManager:enqueueStaticPacenoteByName('firstnoteoutro_1/c')
+  -- self.pinOut.flow.value = true
+  -- self.pinOut.done.value = true
 end
 
-function C:work()
-  if self.pinIn.reset.value then
-    self:reset()
-  end
-
-  if not self.workDone then
-    local loaded = extensions.isExtensionLoaded("gameplay_aipacenotes")
-    if loaded and extensions.gameplay_aipacenotes.isReady() then
-      local rallyManager = extensions.gameplay_aipacenotes.getRallyManager()
-
-      rallyManager.audioManager:enqueuePauseSecs(0.5)
-      rallyManager.audioManager:enqueueStaticPacenoteByName('firstnoteintro_1/c')
-      local pacenote = extensions.gameplay_aipacenotes.getRallyManager().notebook.pacenotes.sorted[1]
-      rallyManager.audioManager:enqueuePacenote(pacenote)
-      rallyManager.audioManager:enqueueStaticPacenoteByName('firstnoteoutro_1/c')
-      self.workDone = true
-      self.pinOut.flow.value = true
-    else
-      self.pinOut.flow.value = false
-    end
-    -- else
-      -- self.pinOut.flow.value = false
-    -- end
-  else
-    self.pinOut.flow.value = true
-  end
-end
+-- function C:work()
+--   if self.pinIn.reset.value then
+--     self:reset()
+--   end
+--
+--   if not self.workDone then
+--     local loaded = extensions.isExtensionLoaded("gameplay_aipacenotes")
+--     if loaded and extensions.gameplay_aipacenotes.isReady() then
+--       local rallyManager = extensions.gameplay_aipacenotes.getRallyManager()
+--
+--       rallyManager.audioManager:enqueuePauseSecs(0.5)
+--       rallyManager.audioManager:enqueueStaticPacenoteByName('firstnoteintro_1/c')
+--       local pacenote = extensions.gameplay_aipacenotes.getRallyManager().notebook.pacenotes.sorted[1]
+--       rallyManager.audioManager:enqueuePacenote(pacenote)
+--       rallyManager.audioManager:enqueueStaticPacenoteByName('firstnoteoutro_1/c')
+--       self.workDone = true
+--       self.pinOut.flow.value = true
+--     else
+--       self.pinOut.flow.value = false
+--     end
+--     -- else
+--       -- self.pinOut.flow.value = false
+--     -- end
+--   else
+--     self.pinOut.flow.value = true
+--   end
+-- end
 
 return _flowgraph_createNode(C)
--- lua/ge/extensions/flowgraph/nodes/gameplay/aiPacenotes/waitForEmptyAudioQueue.lua
+++ lua/ge/extensions/flowgraph/nodes/gameplay/aiPacenotes/waitForEmptyAudioQueue.lua
@@ -8,31 +8,63 @@ C.description = 'Waits for audio queue to be empty'
 C.color = re_util.aip_fg_color
 
 C.pinSchema = {
-  { dir = 'in', type = 'flow', name = 'flow', description = 'Inflow for this node.' },
+  -- { dir = 'in', type = 'flow', name = 'activate', description = 'Inflow for this node.', impulse = true },
+  { dir = 'in', type = 'flow', name = 'reset', description = 'Resets this node.', impulse = true },
   -- { dir = 'in', type = 'flow', name = 'reset', description = 'Reset the countdown.', impulse = true },
   { dir = 'out', type = 'flow', name = 'flow', description = 'Outflow for this node.' },
+  { dir = 'out', type = 'flow', name = 'empty', description = 'flows when empty' },
 }
 C.tags = {'aipacenotes'}
+C.category = 'repeat_instant'
 
-function C:init(mgr, ...)
-end
-
--- function C:reset()
-  -- self.pinIn.reset.value = false
-  -- self.pinOut.flow.value = false
-  -- self.found_empty = false
-  -- self.found_non_empty = false
+-- function C:init(mgr, ...)
 -- end
+
+function C:reset()
+  self.pinIn.reset.value = false
+  self.pinOut.flow.value = false
+  self.pinOut.empty.value = false
+  self.found_empty = false
+  -- self.found_non_empty = false
+end
 
 -- function C:_executionStopped()
 --   self:reset()
 -- end
 
+-- function C:work(args)
+--   -- print(self.pinIn.reset.value)
+--   -- if self.pinIn.flow.value then
+--   --   print('pinIn.flow: true')
+--   -- end
+--   if self.pinIn.reset.value then
+--     print('pinIn.reset: true')
+--   end
+--   self.pinOut.flow.value = false
+-- end
+
 function C:work(args)
-  -- if self.pinIn.reset.value then
-  --   print('reset')
-  --   self:reset()
-  -- end
+  if self.pinIn.reset.value then
+    print('pinIn.reset: true')
+    self:reset()
+    return
+  end
+
+  if not self.found_empty then
+    print('not found_empty')
+    local qs = extensions.gameplay_aipacenotes.getRallyManager().audioManager:getQueueInfo()
+    if qs.queueSize == 0 and qs.paused then
+      self.found_empty = true
+      self.pinOut.empty.value = true
+    end
+  else
+    print('found_empty')
+    -- self.pinOut.flow.value = true
+  end
+
+  self.pinOut.flow.value = true
+
+  -- self.pinOut.flow.value = false
 
   -- if not self.found_non_empty then
   --   local qs = extensions.gameplay_aipacenotes.getRallyManager().audioManager:getQueueInfo()
@@ -47,7 +79,7 @@ function C:work(args)
   --   else
   --     local qs = extensions.gameplay_aipacenotes.getRallyManager().audioManager:getQueueInfo()
   --     if qs.queueSize == 0 and qs.paused then
-  --     print('found_empty')
+  --       print('found_empty')
   --       self.found_empty = true
   --       self.pinOut.flow.value = true
   --     else
@@ -56,14 +88,14 @@ function C:work(args)
   --   end
   -- end
 
-  local qs = extensions.gameplay_aipacenotes.getRallyManager().audioManager:getQueueInfo()
-  -- if qs.queueSize > 0 or (qs.queueSize == 0 and not qs.paused) then
+  -- local qs = extensions.gameplay_aipacenotes.getRallyManager().audioManager:getQueueInfo()
+  -- -- if qs.queueSize > 0 or (qs.queueSize == 0 and not qs.paused) then
+  -- -- end
+  -- if qs.queueSize == 0 and qs.paused then
+  --   self.pinOut.flow.value = true
+  -- else
+  --   self.pinOut.flow.value = false
   -- end
-  if qs.queueSize == 0 and qs.paused then
-    self.pinOut.flow.value = true
-  else
-    self.pinOut.flow.value = false
-  end
 end
 
 return _flowgraph_createNode(C)
