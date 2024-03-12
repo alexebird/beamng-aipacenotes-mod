55ea18485592e5b5041231be1e17b61ee165e978
Alex Bird <alexebird@gmail.com>
2024-03-11 23:27:55 -0700

    update conte marlo notes

--- build.sh
+++ build.sh
@@ -5,11 +5,31 @@ set -euo pipefail
 main() {
     if git diff-index --quiet HEAD --; then
         # Working tree is clean
-        echo "$(git rev-parse HEAD)" > gitsha.txt
+        sha=$(git rev-parse HEAD)
+        # Attempt to get the tag name exactly on HEAD
+        tag=$(git describe --tags --exact-match HEAD 2> /dev/null)
+        if [ -n "$tag" ]; then
+            # If there is a tag exactly on HEAD, append it to the SHA
+            echo "${sha} (${tag})" > gitsha.txt
+        else
+            # If no tag on HEAD, just write the SHA
+            echo "$sha" > gitsha.txt
+        fi
     else
         # Working tree is not clean
-        echo "$(git rev-parse HEAD)+dirty" > gitsha.txt
+        sha=$(git rev-parse HEAD)
+        # Attempt to get the tag name exactly on HEAD
+        tag=$(git describe --tags --exact-match HEAD 2> /dev/null)
+        if [ -n "$tag" ]; then
+            # If there is a tag exactly on HEAD, append it to the SHA and mark as dirty
+            echo "${sha}+dirty (${tag})" > gitsha.txt
+        else
+            # If no tag on HEAD, just write the SHA and mark as dirty
+            echo "${sha}+dirty" > gitsha.txt
+        fi
     fi
+
+    cat gitsha.txt
 
     zip -r ../aipacenotes.zip ./* -x '*.git*' -x 'art*.png' -x 'docs' -x 'build.sh'
     rm -fv gitsha.txt
