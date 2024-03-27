1760776896aeb769d4efe65c317f554925808a32
Alex Bird <alexebird@gmail.com>
2024-03-26 18:37:57 -0700

    remove keybinding action for toggle drawdebug of pacenotes in recce, add back echo canyon and forbin pines

--- lua/ge/extensions/gameplay/aipacenotes/inputActions.lua
+++ lua/ge/extensions/gameplay/aipacenotes/inputActions.lua
@@ -33,11 +33,11 @@ local function action_transcribe_recording_cut()
   guihooks.trigger('aiPacenotesInputActionCutRecording')
 end
 
-local function action_toggle_recce_drawDebug()
-  log('I', logTag, 'action_toggle_recce_drawDebug')
-  if not is_recceApp_loaded() then return end
-  guihooks.trigger('aiPacenotesInputActionToggleDrawDebug')
-end
+-- local function action_toggle_recce_drawDebug()
+--   log('I', logTag, 'action_toggle_recce_drawDebug')
+--   if not is_recceApp_loaded() then return end
+--   guihooks.trigger('aiPacenotesInputActionToggleDrawDebug')
+-- end
 
 local function action_recce_move_pacenote_forward()
   log('I', logTag, 'action_recce_move_pacenote_forward')
@@ -74,12 +74,12 @@ end
 
 -- M.action_transcribe_recording_start = action_transcribe_recording_start
 -- M.action_transcribe_recording_stop = action_transcribe_recording_stop
+-- M.action_toggle_recce_drawDebug = action_toggle_recce_drawDebug
+-- M.action_recce_insert_mode = action_recce_insert_mode
 M.action_transcribe_recording_cut = action_transcribe_recording_cut
-M.action_toggle_recce_drawDebug = action_toggle_recce_drawDebug
 M.action_recce_move_pacenote_forward = action_recce_move_pacenote_forward
 M.action_recce_move_pacenote_backward = action_recce_move_pacenote_backward
 M.action_recce_move_vehicle_forward = action_recce_move_vehicle_forward
 M.action_recce_move_vehicle_backward = action_recce_move_vehicle_backward
--- M.action_recce_insert_mode = action_recce_insert_mode
 
 return M
