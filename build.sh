dbe8df2b0e09fda8fc38fd028f1c974e6ba61a52
Alex Bird <alexebird@gmail.com>
2024-05-12 07:39:05 -0700

    add vehicles and missions back in: med stages, castelletto, sorland

--- build.sh
+++ build.sh
@@ -4,27 +4,32 @@ set -euo pipefail
 
 main() {
     if git diff-index --quiet HEAD --; then
-        # Working tree is clean
+        echo Working tree is clean
         sha=$(git rev-parse HEAD)
         # Attempt to get the tag name exactly on HEAD
         tag=$(git describe --tags --exact-match HEAD 2> /dev/null)
         if [ -n "$tag" ]; then
             # If there is a tag exactly on HEAD, append it to the SHA
+            echo "${sha} (${tag})"
             echo "${sha} (${tag})" > version.txt
         else
             # If no tag on HEAD, just write the SHA
+            echo "$sha"
             echo "$sha" > version.txt
         fi
     else
-        # Working tree is not clean
+        echo Working tree is not clean
         sha=$(git rev-parse HEAD)
         # Attempt to get the tag name exactly on HEAD
-        tag=$(git describe --tags --exact-match HEAD 2> /dev/null)
+        tag=$(git describe --tags HEAD 2> /dev/null)
+        # echo foo
         if [ -n "$tag" ]; then
             # If there is a tag exactly on HEAD, append it to the SHA and mark as dirty
+            echo "${sha}+dirty (${tag})"
             echo "${sha}+dirty (${tag})" > version.txt
         else
             # If no tag on HEAD, just write the SHA and mark as dirty
+            echo "${sha}+dirty"
             echo "${sha}+dirty" > version.txt
         fi
     fi
